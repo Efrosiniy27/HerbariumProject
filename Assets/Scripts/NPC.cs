17891a688795e92f6b2cee666ecedfc54b8a01f5
Ilya <efrosiniy1998@gmail.com>
2020-09-01 11:51:08 +0300

    first commit

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC : MonoBehaviour
+{
+    public float _Speed = 1.0f;
+    public float _amplitude = 5.0f;
+    public Animator _animator;
+    public Text _textDialog;
+    private Transform _transform;
+    private Transform _transformSprite;  
+    private float _startPosX;
+    private float _dir=1;
+    private List<string> _Dialog=new List<string>() {" Да пошел ты." , "И не надейся.",};
+    private string _endDialog= "Мы с тобой уже все решили";
+    private delegate void Motion();
+    Motion motion;
+    private void Awake()
+    {        
+        _transform = GetComponent<Transform>();
+        _startPosX = _transform.position.x;
+        _transformSprite = _animator.gameObject.GetComponent<Transform>();      
+    }
+    private void Start()
+    {
+        motion = Go;
+        _animator.Play("GoGG");
+    }
+    void Update()
+    {
+      if (motion!=null) motion();       
+    }
+    public void Go()
+    {    
+        if (ExitBound())
+        {
+            _dir *= -1;
+            _transformSprite.localScale = new Vector3(_dir, 1, 1);            
+        }
+        _transform.position += new Vector3(_dir, 0, 0) * _Speed * Time.deltaTime;
+    }
+    public bool ExitBound()
+    {
+        return (_transform.position.x > (_startPosX + _amplitude)) || (_transform.position.x < (_startPosX - _amplitude));
+    }
+    public void Ask()
+    {        
+        motion = null;
+        _textDialog.gameObject.SetActive(true);
+        _animator.Play("IdleGG");
+        if (_Dialog.Count == 0)
+        {         
+            _textDialog.gameObject.SetActive(false);
+            EndAsk();
+            return;
+        }        
+        _textDialog.text = _Dialog[0];
+        _Dialog.RemoveAt(0);
+    }
+    public void EndAsk()
+    {
+        _textDialog.gameObject.SetActive(true);
+        _textDialog.text = _endDialog;
+        StartCoroutine(EndAskCor());
+    }
+    IEnumerator EndAskCor()
+    {
+        yield return new WaitForSeconds(3.0f);
+        _textDialog.gameObject.SetActive(false);        
+        motion = Go;
+        _animator.Play("GoGG");
+    }
+}
---
+++ Assets/Scripts/PlayerGG.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerGG : MonoBehaviour
+{
+    public float _Speed = 1.0f;
+    public Animator _animator;
+    public Text _textDialog;
+    private GameObject _itemMotion;
+    private Transform _transform;
+    private Transform _transformSprite;
+    private List<string> _Dialog = new List<string>() { "Слушай, я не хотел ничего воровать, бригадир меня заставил.  ", "Надеюсь мы ещё сможем найти общий язык…  "};
+    private void Awake()
+    {
+        _transform = GetComponent<Transform>();
+        _transformSprite = _animator.gameObject.GetComponent<Transform>();
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            MovePlayer(-1.0f);
+            return;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            MovePlayer(1.0f);
+            return;
+        }
+        if (Input.GetKeyDown(KeyCode.E)&&(_itemMotion!=null))
+        {
+            ShowText();
+            _itemMotion.GetComponent<NPC>().Ask();
+        }
+        _animator.Play("IdleGG");
+    }
+    public void MovePlayer(float dir)
+    {
+        _transform.position += new Vector3(dir, 0, 0) * _Speed * Time.deltaTime;
+        _transformSprite.localScale = new Vector3(dir, 1, 1);
+        _animator.Play("GoGG");
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        _itemMotion = collision.gameObject;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        _itemMotion = null;
+    }
+    public void ShowText()
+    {
+        if (_Dialog.Count == 0)
+        {
+            _textDialog.gameObject.SetActive(false);
+            return;
+        }
+        _textDialog.gameObject.SetActive(true);
+        _textDialog.text = _Dialog[0];
+        _Dialog.RemoveAt(0);
+    }
+}
